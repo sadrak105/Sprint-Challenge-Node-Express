c481da6555bfb615b027aa0eb91d89ffc5f25703
sadrak105 <sadrak10@gmail.com>
2018-05-04 11:46:54 -0400

    added get funtionality to projects

--- projects/projectRoutes.js
+++ projects/projectRoutes.js
@@ -4,6 +4,31 @@ const db = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
+//===================GET================
+router.get('/', (req, res) => {
+    db
+    .get()
+    .then(projects => {
+        res.json(projects);
+    })
+    .catch(err => {
+        res.status(500).json({ error:"The projects information could not load." })
+    })
+})
+
+router.get('/:id', (req,res) => {
+
+    db
+    .get(req.params.id)
+    .then(projects => {
+            res.status(200).json(projects);
+    })
+    .catch(err => {
+        res.status(500).json({error: "The project information could not be retrieved."})
+    })
+});
+
+//==================PUT====================
 
 
 
