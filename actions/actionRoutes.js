b4756c503a0de4428be8943dfb4a10b85be03d87
sadrak105 <sadrak10@gmail.com>
2018-05-04 14:23:46 -0400

    actions post

--- actions/actionRoutes.js
+++ actions/actionRoutes.js
@@ -4,6 +4,49 @@ const db = require('../data/helpers/actionModel');
 
 const router = express.Router();
 
+//=================POST====================
+router.post('/', (req, res) =>{
+    const { project_id, description, notes } = req.body;
+    const newAction = req.body
+
+    if (!newAction.project_id || !newAction.description){
+        res.status(400).json({ error:"Required Fields!"})
+    }
+    if (newAction.description.length > 128){
+        res.status(400).json({ error:"Has To Be Less Than 128 Characters" })
+    }
+    else {
+        db
+        .insert(newAction)
+        .then(actions => {
+            res.status(201).json(actions)
+        })
+    }
+})
+
+//=============GET==================
+router.get('/', (req, res) => {
+    db
+    .get()
+    .then(actions => {
+        res.status(200).json(actions);
+    })
+    .catch(err => {
+        res.status(500).json({ error:"Error Getting Actions" })
+    })
+})
+
+//=======GET BY ID==========
+router.get('/:id', (req, res) => {
+    db
+    .get(req.params.id)
+    .then(actions => {
+        res.status(200).json(actions);
+    })
+    .catch(err => {
+        res.status(500).json({ error:"Error Getting Actions By Id" })
+    })
+})
 
 
 
