ebdcc26c27d1d4315a0d32f5f3fae7217b8008b4
sadrak105 <sadrak10@gmail.com>
2018-05-04 09:53:23 -0400

    used dependencies

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+
