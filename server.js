d85a9c2106acfe34154ffd4650b91c440730242d
sadrak105 <sadrak10@gmail.com>
2018-05-04 10:08:57 -0400

    setup routes on server

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const actionRoutes = require('./actions/actionRoutes');
+const projectRoutes =require('./projects/projectRoutes');
+
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+
+server.use('/api/actions', actionRoutes);
+server.use('/api/projects', projectRoutes);
 
 
 const port = 5000;
